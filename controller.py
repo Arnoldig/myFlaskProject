72bed5adfcdf7ecce42d0ed1a2933844179c30f7
Arnoldig <codergit@yandex.ru>
2024-05-12 18:07:33 +0300

    Изучение Flask по урокам selfedu. Проведена работа с куки и сессиями. Подготовлены материалы для проработки с наставником.

--- controller.py
+++ controller.py
@@ -1,5 +1,4 @@
-from werkzeug.security import generate_password_hash
-from flask import render_template, request
+from flask import render_template, request, make_response, session
 from main import app
 from model import User, Profiles, Message
 
@@ -25,21 +24,6 @@ def registration():
                            city=request.form['city'],
                            user_id=u_id)
 
-        # старая раелазация
-        # try:
-        #     hash = generate_password_hash(request.form['password'])
-        #     u = User(email=request.form['email'], psw=hash)
-        #     db.session.add(u)
-        #     db.session.flush()
-        #
-        #     p = Profiles(name=request.form['name'], old=request.form['old'],
-        #                  city=request.form['city'], user_id=u.id)
-        #     db.session.add(p)
-        #     db.session.commit()
-        # except:
-        #     db.session.rollback()
-        #     print('Ошибка добавления данных в базу данных')
-
     return render_template('registration.html',
                            title='Регистрация новых пользователей',
                            menu=menu)
@@ -48,14 +32,49 @@ def registration():
 @app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html', title='О сайте!',
-                           menu=menu)
+    session.permanent = True # указываем браузеру что сессию нужно сохранить
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
+
+    if 'data' in session:
+        session['data'][1] += 1
+        session.modified = True  # работает и без этой строчки ...
+    else:
+        session['data'] = [1, 2, 3, 4]
+
+    return render_template('index.html',
+                           title='О сайте!',
+                           menu=menu,
+                           visits=session['visits'], data=session['data'])
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='О нас!',
-                           menu=menu)
+    content = render_template('about.html', title='О нас!',
+                              menu=menu)
+    res = make_response(content, 200)
+    res.headers['Content-Type'] = 'text/html'
+    res.headers['Server'] = 'flask'
+    return res
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    log = '-'
+    if request.cookies.get('logged_key'):
+        log = request.cookies.get('logged_key')
+
+    text = f'<p>logged_key: {log} </p>'
+    content = render_template('404.html',
+                              title='Страницы не существует!',
+                              menu=menu,
+                              cookie=text)
+
+    res = make_response(content, 404)
+    res.set_cookie('logged_key', 'yes', max_age=1 * 24 * 60 * 60)
+    return res
 
 
 @app.route('/contact', methods=['GET', 'POST'])
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f94753a248dce5c430c61430bb034c09d5cc7cb1'
+app.permanent_session_lifetime = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
