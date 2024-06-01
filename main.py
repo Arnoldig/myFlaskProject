2e104ada99c804f477f9566d45ba0ff4edad7123
Arnoldig <codergit@yandex.ru>
2024-06-01 10:58:27 +0300

    Создан раздел под админку через blueprint

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from personal.personal import personal
+
 
 app = Flask(__name__)
+app.register_blueprint(personal, url_prefix='/personal')
 app.config['SECRET_KEY'] = 'f94753a248dce5c430c61430bb034c09d5cc7cb1'
 app.permanent_session_lifetime = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
